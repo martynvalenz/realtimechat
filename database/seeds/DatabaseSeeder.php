17e0ac6329ff5879645a8b250ebdbc26263d4bca
Martín Valenzuela Magallanes <martynvalenz@gmail.com>
2020-06-28 22:10:57 -0600

    FActory users

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,23 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(User::class)->create(
+            [
+                'email' => 'martynvalenz@gmail.com',
+                'name' => 'Martin Valenzuela'
+            ]
+        );
+        factory(User::class)->create(
+            [
+                'email' => 'ankur@gmail.com',
+                'name' => 'Ankur Shrivastava'
+            ]
+        );
+        factory(User::class)->create(
+            [
+                'email' => 'marvin.torrentera@gmail.com',
+                'name' => 'Marvin Torrentera'
+            ]
+        );
     }
 }
